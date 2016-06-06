5a022a14bb76541d23ada35c7b0646218c35a4ba
Mohaideen3m <mohaideen.3mm1@live.com>
2016-06-06 22:40:59 +0530

    Rename reverse.py to reversestring.py

---
+++ reversestring.py
@@ -0,0 +1,5 @@
+str1=input()
+output1=str1[::-1]
+print("output1:"+output1)
+output2=output1.replace('a','').replace('e','').replace('i','').replace('o','').replace('u','')
+print("output2:"+output2)
