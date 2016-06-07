1fcb7419bad7f110022cd177da07a86954ca1c46
Mohaideen3m <mohaideen.3mm1@live.com>
2016-06-07 12:36:57 +0530

    Create equalindex.py

---
+++ equalindex.py
@@ -0,0 +1,12 @@
+arr=[]
+n=int(input("Enter the size"))
+print("the numbers should be unique")
+for i in range(0,n):
+	v=int(input())
+	arr.append(v)
+arr1=sorted(arr)
+print(arr1)
+for i in range(0,n):
+	if(i==arr1[i]):
+		print("the number %d equal its index %d"%(i,arr1[i]))
+		
